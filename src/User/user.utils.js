49036326bfeba5c558806202c5ada70ea83ef127
ezdar <ezdar2743@gmail.com>
2022-05-11 10:30:11 +0900

    protect loggedInUser and add currentUser schema

---
+++ src/User/user.utils.js
@@ -0,0 +1,22 @@
+import jsonwebtoken from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import client from "../client";
+
+export const getUser = async (Authorization) => {
+  try {
+    if (!Authorization) {
+      return null;
+    }
+    const { id } = await jwt.verify(Authorization, process.env.SECRET_KEY);
+    const user = await client.user.findUnique({
+      where: { id },
+    });
+    if (user) {
+      return user;
+    } else {
+      return null;
+    }
+  } catch {
+    return null;
+  }
+};
